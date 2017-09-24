a56f5efe57a4b150c49dbdb46dcfa4f912fa7768
Leandro Teodoro <leandroteodoro.rj@gmail.com>
2017-09-24 07:35:14 -0300

    Add files via upload

--- MicrochipPicDisplay7Segmentos/main.c
+++ MicrochipPicDisplay7Segmentos/main.c
@@ -41,9 +41,65 @@ int   w_temp;
 int   status_temp;
 long int   count;
 unsigned long int   count_timer1;
-int   CONV_DISP[25];
 int   count_disp_control;
 int   count_disp;
+const int   CONV_DISP[25]={                   
+                   0b00111111,
+                   0b00000110,
+                   0b01011011,
+                   0b01001111,
+                   0b01100110,
+                   0b01101101,
+                   0b01111100,
+                   0b00000111,
+                   0b01111111,
+                   0b01101111,
+                   0b01110111,
+                   0b00111001,
+                   0b01011110,
+                   0b01111001,
+                   0b01110001,
+                   0b01111101,
+                   0b01110110,
+                   0b00011110,
+                   0b00111000,
+                   0b01010100,
+                   0b01011100,
+                   0b01110011,
+                   0b01010000,
+                   0b01111000,
+                   0b00011100
+                           };
+/*
+      //Conversão para o display de 7 segmentos
+      //      LEDS:  PGFEDCBA        Onde P=ponto
+      CONV_DISP[0]=0b00111111; //0
+      CONV_DISP[1]=0b00000110; //1
+      CONV_DISP[2]=0b01011011; //2
+      CONV_DISP[3]=0b01001111; //3
+      CONV_DISP[4]=0b01100110; //4
+      CONV_DISP[5]=0b01101101; //5
+      CONV_DISP[6]=0b01111100; //6
+      CONV_DISP[7]=0b00000111; //7
+      CONV_DISP[8]=0b01111111; //8
+      CONV_DISP[9]=0b01101111; //9
+     CONV_DISP[10]=0b01110111; //A
+     CONV_DISP[11]=0b00111001; //C
+     CONV_DISP[12]=0b01011110; //d
+     CONV_DISP[13]=0b01111001; //E
+     CONV_DISP[14]=0b01110001; //F
+     CONV_DISP[15]=0b01111101; //G
+     CONV_DISP[16]=0b01110110; //H
+     CONV_DISP[17]=0b00011110; //J
+     CONV_DISP[18]=0b00111000; //L
+     CONV_DISP[19]=0b01010100; //n
+     CONV_DISP[20]=0b01011100; //o
+     CONV_DISP[21]=0b01110011; //P
+     CONV_DISP[22]=0b01010000; //r
+     CONV_DISP[23]=0b01111000; //t
+     CONV_DISP[24]=0b00011100; //u
+*/
+
 
 //***************************************************************************
 //                               DRIVES
@@ -161,36 +217,6 @@ void main(){
       count_timer1 = 0;
       count_disp_control = 0;
       count_disp = 0;
-      
-      //Conversão para o display de 7 segmentos
-      //      LEDS:  PGFEDCBA        Onde P=ponto
-      CONV_DISP[0]=0b00111111; //0
-      CONV_DISP[1]=0b00000110; //1
-      CONV_DISP[2]=0b01011011; //2
-      CONV_DISP[3]=0b01001111; //3
-      CONV_DISP[4]=0b01100110; //4
-      CONV_DISP[5]=0b01101101; //5
-      CONV_DISP[6]=0b01111100; //6
-      CONV_DISP[7]=0b00000111; //7
-      CONV_DISP[8]=0b01111111; //8
-      CONV_DISP[9]=0b01101111; //9
-     CONV_DISP[10]=0b01110111; //A
-     CONV_DISP[11]=0b00111001; //C
-     CONV_DISP[12]=0b01011110; //d
-     CONV_DISP[13]=0b01111001; //E
-     CONV_DISP[14]=0b01110001; //F
-     CONV_DISP[15]=0b01111101; //G
-     CONV_DISP[16]=0b01110110; //H
-     CONV_DISP[17]=0b00011110; //J
-     CONV_DISP[18]=0b00111000; //L
-     CONV_DISP[19]=0b01010100; //n
-     CONV_DISP[20]=0b01011100; //o
-     CONV_DISP[21]=0b01110011; //P
-     CONV_DISP[22]=0b01010000; //r
-     CONV_DISP[23]=0b01111000; //t
-     CONV_DISP[24]=0b00011100; //u
-      
-      
       
 //****************************************************************************
 //                             INICIO DO LOOP
